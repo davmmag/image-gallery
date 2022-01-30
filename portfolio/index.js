d33a132bd581701152cb82548285a3d1dc8584f4
Murat Davletbakov <goodofdmm@gmail.com>
2022-01-30 21:17:12 +0500

    feat: add translater

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,31 +1,33 @@
-const menu = document.querySelector('.menu-btn');
-const themeBtn = document.querySelector('.header__theme');
+const MENU = document.querySelector('.menu-btn');
+const THEME_BTN = document.querySelector('.header__theme');
+const BTN_CONTROLS = document.querySelectorAll('.portfolio__btn');
+const IMAGES = document.querySelectorAll('.portfolio__img img');
 
-
-menu.addEventListener('click', e => {
-    activeMenu(e);
-});
-const activeMenu = e => {
-    e.preventDefault();
-    menu.classList.toggle('menu-btn__active');
+//Menu 
+const activeMenu = (event, element) => {
+    event.preventDefault();
+    element.classList.toggle('menu-btn__active');
     document.querySelector('.burger').classList.toggle('burger-active')
-};
+}
+
+MENU.addEventListener('click', e => {
+    activeMenu(e, MENU);
+});
+
+//Theme
+document.addEventListener('DOMContentLoaded', () => {
+    document.documentElement.className = localStorage.getItem('theme');
+})
 const setTheme = (theme) => {
     localStorage.setItem('theme', theme);
     document.documentElement.className = theme;
 }
 const changeTheme = () => localStorage.getItem('theme') === 'dark' ? setTheme('light') : setTheme('dark');
-setTheme('dark');
-themeBtn.addEventListener('click', () => {
-    changeTheme();
-})
+THEME_BTN.addEventListener('click', changeTheme);
+
 
 
 //Portfolio
-
-const BTN_CONTROLS = document.querySelectorAll('.portfolio__btn');
-const IMAGES = document.querySelectorAll('.portfolio__img img');
-
 const selectImages = (btns, images) => {
     for(let btn of btns) {
         btn.addEventListener('click', e => {
@@ -39,3 +41,36 @@ const selectImages = (btns, images) => {
 }
 
 selectImages(BTN_CONTROLS, IMAGES);
+
+//Change language
+import i18Obj from './translate.js';
+const BTNS_LANG = document.querySelectorAll('.btn__lang');
+const TRANSLATE_ITEMS = document.querySelectorAll('[data-i18]');
+
+// for(let item in i18Obj) {
+//     console.log(item)
+// }
+
+const getTranslate = (lang) => {
+    let trs = Array.from(TRANSLATE_ITEMS) 
+    for(let key in i18Obj[lang]) {
+         for(let item of trs) {
+             if(item.dataset.i18 == key) {
+                if(item.placeholder) item.placeholder = i18Obj[lang][key];
+                item.textContent = i18Obj[lang][key];
+             }
+         }
+    }
+}
+
+for(let btn of BTNS_LANG) {
+    btn.addEventListener('click', e => {
+        if (e.target.dataset.lang === 'russian') {
+            getTranslate('ru');
+        } else {
+            getTranslate('en')
+        }
+    })
+}
+
+
