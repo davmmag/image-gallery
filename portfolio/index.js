d7f303fd5d95e34986a3fa883fdfa85408e5540f
davmmag <96019287+davmmag@users.noreply.github.com>
2022-01-31 19:53:48 +0300

    feat: add  assessment

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,7 +1,10 @@
+import i18Obj from './translate.js';
 const MENU = document.querySelector('.menu-btn');
 const THEME_BTN = document.querySelector('.header__theme');
 const BTN_CONTROLS = document.querySelectorAll('.portfolio__btn');
 const IMAGES = document.querySelectorAll('.portfolio__img img');
+const BTNS_LANG = document.querySelectorAll('.btn__lang');
+const TRANSLATE_ITEMS = document.querySelectorAll('[data-i18]');
 
 //Menu 
 const activeMenu = (event, element) => {
@@ -10,10 +13,8 @@ const activeMenu = (event, element) => {
     document.querySelector('.burger').classList.toggle('burger-active')
 }
 
-MENU.addEventListener('click', e => {
-    activeMenu(e, MENU);
-});
-
+MENU.addEventListener('click', e => activeMenu(e, MENU))
+   
 //Theme
 document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.className = localStorage.getItem('theme');
@@ -24,8 +25,6 @@ const setTheme = (theme) => {
 }
 const changeTheme = () => localStorage.getItem('theme') === 'dark' ? setTheme('light') : setTheme('dark');
 THEME_BTN.addEventListener('click', changeTheme);
-
-
 
 //Portfolio
 const selectImages = (btns, images) => {
@@ -43,14 +42,6 @@ const selectImages = (btns, images) => {
 selectImages(BTN_CONTROLS, IMAGES);
 
 //Change language
-import i18Obj from './translate.js';
-const BTNS_LANG = document.querySelectorAll('.btn__lang');
-const TRANSLATE_ITEMS = document.querySelectorAll('[data-i18]');
-
-// for(let item in i18Obj) {
-//     console.log(item)
-// }
-
 const getTranslate = (lang) => {
     let trs = Array.from(TRANSLATE_ITEMS) 
     for(let key in i18Obj[lang]) {
@@ -72,5 +63,9 @@ for(let btn of BTNS_LANG) {
         }
     })
 }
-
+console.log(`Смена изображений в секции portfolio +25
+            Перевод страницы на два языка +25
+            Переключение светлой и тёмной темы +25
+            Дополнительный функционал: выбранный пользователем язык отображения страницы и светлая или тёмная тема сохраняются при перезагрузке страницы +5
+            Дополнительный функционал: сложные эффекты для кнопок при наведении и/или клике +5`);
 
