0c9d9ffafa06963118d9ea9261f98461dda60f80
Murat Davletbakov <goodofdmm@gmail.com>
2023-03-25 13:15:46 +0500

    refactor: refactor js code

--- image-galery/src/index.js
+++ image-galery/src/index.js
@@ -6,10 +6,10 @@ const CLEAR_ICON = document.querySelector('.search__close-icon');
 const BTN_SEARCH = document.querySelector('.search__icon');
 let URL = `https://api.unsplash.com/photos?page=2&per_page=21&client_id=wdMiGp849PdPDqo6Dbf8rws33Fwjfjz8qjknrDVbs_U`;
 
-const createImg = (src, name, container) => {
+const createImg = (src, name, container, title) => {
   let img = document.createElement('div');
   img.classList.add(name);
-  img.title = src.description;
+  img.title = title;
   img.style.backgroundImage = `url(${src})`;
   container.append(img);
   img.addEventListener('click', () => window.open(src, '_blank'));
@@ -22,11 +22,11 @@ const getPhotos = async (url) => {
       let data = await RESPONSE.json();
       if(data.results) {
         for (let item of data.results) { 
-          createImg(`${item.urls.raw}&w=1366&h=768`, 'content__img', CONTENT);
+          createImg(`${item.urls.raw}&w=1366&h=768`, 'content__img', CONTENT, item.alt_description);
         }
       } else {
         for (let item in data) {
-          createImg(`${data[item].urls.raw}&w=1366&h=768`, 'content__img', CONTENT);
+          createImg(`${data[item].urls.raw}&w=1366&h=768`, 'content__img', CONTENT, data[item].alt_description);
         }
       }
     }
@@ -53,12 +53,12 @@ const clearSearchValue = (deleteValue, deleteButton, searchBlock) => {
 window.addEventListener('load', () => getPhotos(URL));
 
 SEARCH.addEventListener('keydown', e => {
-    if (e.key === 'Enter') updateGallery();
+  if (e.key === 'Enter') updateGallery();
 });
 
 SEARCH.addEventListener('input', () =>  {
-    clearSearchValue(null, CLEAR_ICON, SEARCH);
-    URL = `https://api.unsplash.com/search/photos?query=${SEARCH.value}&per_page=21&client_id=wdMiGp849PdPDqo6Dbf8rws33Fwjfjz8qjknrDVbs_U`;
+  clearSearchValue(null, CLEAR_ICON, SEARCH);
+  URL = `https://api.unsplash.com/search/photos?query=${SEARCH.value}&per_page=21&client_id=wdMiGp849PdPDqo6Dbf8rws33Fwjfjz8qjknrDVbs_U`;
 });
 
 CLEAR_ICON.addEventListener('click', () => clearSearchValue(true, CLEAR_ICON, SEARCH));
